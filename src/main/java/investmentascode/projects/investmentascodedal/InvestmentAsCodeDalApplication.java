0309e4a2365fe7562c445fb76daa047277c8ccba
MaxMA2000 <maxmahklife@gmail.com>
2024-03-20 12:26:22 +0800

    setup(main): set up the springboot project structure for DAL

---
+++ src/main/java/investmentascode/projects/investmentascodedal/InvestmentAsCodeDalApplication.java
@@ -0,0 +1,13 @@
+package investmentascode.projects.investmentascodedal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentAsCodeDalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InvestmentAsCodeDalApplication.class, args);
+  }
+
+}
---
+++ src/test/java/investmentascode/projects/investmentascodedal/InvestmentAsCodeDalApplicationTests.java
@@ -0,0 +1,13 @@
+package investmentascode.projects.investmentascodedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAsCodeDalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
