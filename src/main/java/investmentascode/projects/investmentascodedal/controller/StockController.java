6d10f521076ed9233fef5270033a990f8a550182
MaxMA2000 <maxmahklife@gmail.com>
2024-03-24 22:35:33 +0800

    feat(IAC-10): add the feature to filter stock data by id

--- src/main/java/investmentascode/projects/investmentascodedal/controller/StockController.java
+++ src/main/java/investmentascode/projects/investmentascodedal/controller/StockController.java
@@ -1,14 +1,18 @@
 package investmentascode.projects.investmentascodedal.controller;
 
 
+import investmentascode.projects.investmentascodedal.exception.CustomException;
 import investmentascode.projects.investmentascodedal.model.Stock;
 import investmentascode.projects.investmentascodedal.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/data/v1/stock")
@@ -26,7 +30,14 @@ public class StockController {
     return stockRepository.findAll();
   }
 
-
+  @GetMapping("/byId")
+  public ResponseEntity<?> getStocksById(@RequestParam("id") long id) {
+    List<Stock> stocks = stockRepository.findByAssetId(id);
+    if (stocks.isEmpty()) {
+      throw new CustomException("ID " + id + " doesn't exist. Please check.");
+    }
+    return ResponseEntity.ok(stocks);
+  }
 
 
 }
--- src/main/java/investmentascode/projects/investmentascodedal/repository/StockRepository.java
+++ src/main/java/investmentascode/projects/investmentascodedal/repository/StockRepository.java
@@ -3,5 +3,9 @@ package investmentascode.projects.investmentascodedal.repository;
 import investmentascode.projects.investmentascodedal.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    List<Stock> findByAssetId(Long assetId);
 }
