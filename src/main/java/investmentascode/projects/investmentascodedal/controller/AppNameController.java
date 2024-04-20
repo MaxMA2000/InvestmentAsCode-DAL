58397d08a98df2a4c9659ae7fa52146795d4bf28
MaxMA2000 <maxmahklife@gmail.com>
2024-04-20 14:12:59 +0800

    fix(controller): fix the / controller

--- src/main/java/investmentascode/projects/investmentascodedal/controller/AppNameController.java
+++ src/main/java/investmentascode/projects/investmentascodedal/controller/AppNameController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/app-name")
+@RequestMapping("/")
 public class AppNameController {
 
   @GetMapping
